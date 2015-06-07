6e2afdfb40d665b6d113642e40e546b3fe3910a0
zhangchi8620 <czhang24@utk.edu>
2015-06-07 17:17:26 -0400

    q1 and q2 both work

--- Questions.java
+++ Questions.java
@@ -12,6 +12,11 @@ public class Questions {
 	// Initialize board, knight, startPint, endPoint
 	public static Knight knight = new Knight();
 	public static Board board = new Board();
+	public static int size;
+	public static int startX;
+	public static int startY;
+	public static int endX;
+	public static int endY;
 	
 	// Define legal moves
 	public final static ArrayList<Move> moves = new ArrayList<Move>();
@@ -26,7 +31,7 @@ public class Questions {
 		moves.add(new Move(2, -1));
 		moves.add(new Move(2, 1));
 				
-
+		
 		// Q1		
 //		questionOne();
 		
@@ -36,13 +41,13 @@ public class Questions {
 	
 	private static void questionOne(ArrayList<Move> steps){
 		// initialize board and knight
-		int size = 8;		
-		int startX = 3;
-		int startY = 3;
-		int endX = 5;
-		int endY = 6;
+		
 		knight = new Knight(startX, startY);
 		board = new Board(size, knight);
+		startX = 1;
+		startY = 1;
+		endX = 8;
+		endY = 8;
 		board.setStartEnd(startX, startY, endX, endY);
 		board.printInitialBoard();
 				
@@ -52,9 +57,9 @@ public class Questions {
 //		steps.add(new Move(-1,2)); 			
 //		steps.add(new Move(-1,2)); 		
 //		steps.add(new Move(2,-1)); 	
-
+		int c = 1;
 		for (Move s : steps){
-			System.out.print("\n*********** Step **********\n");			
+			System.out.printf("\n*********** Step %d **********\n", c++);			
 			if (!knight.validMove(s, board)){
 				System.out.println("Q1 Answer: Invalide sequence.");
 				return;
@@ -62,6 +67,7 @@ public class Questions {
 			else{
 				knight.acceptMove(s, board);
 				knight.printKnight();
+				s.printMove();
 				board.printBoard();
 			}
 		}
@@ -70,11 +76,11 @@ public class Questions {
 	
 	private static void questionTwo(){
 		// initialize board and knight
-		int size = 8;		
-		int startX = 1;
-		int startY = 1;
-		int endX = 8;
-		int endY = 8;
+		size = 8;		
+		startX = 1;
+		startY = 1;
+		endX = 8;
+		endY = 8;
 		knight = new Knight(startX, startY);		
 		board = new Board(size, knight);
 		board.setSquareCount(startX, startY, 1);
@@ -82,29 +88,43 @@ public class Questions {
 		board.printInitialBoard();
 				
 		// create a sequence of steps 
-		ArrayList<Move> steps = new ArrayList<Move>();		
+		ArrayList<Square> steps = new ArrayList<Square>();		
 		
 		findValidPath(steps, startX, startY, endX, endY, 2);
 		board.printBoardCount();
 		
 	    Collections.reverse(steps);
-
-		System.out.println("Valid Path:");
-		for (Move m : steps){
-			System.out.printf("%d, %d\n", m.x, m.y);
+		System.out.println("Find Path:");
+		int c = 1;
+		for (Square m : steps){
+			System.out.printf("%d: %d, %d\n", c++, m.x, m.y);
 		}
 		
-		questionOne(steps);
+		ArrayList<Move> moves = steps2moves(startX, startY, steps);
+//		questionOne(moves);
 
 	}
 	
-	private static boolean findValidPath(ArrayList<Move> steps, int startX, int startY, int endX, int endY, int count){
-		board.printBoardCount();
+	private static ArrayList<Move> steps2moves(int startx, int starty, ArrayList<Square> steps) {
+		ArrayList<Move> moves = new ArrayList<Move>();
+		steps.add(0, new Square(startx, starty));
+		int c = 1;
+		for (int i = 0; i < steps.size()-1; i++){
+			Move m = new Move(steps.get(i+1).x-steps.get(i).x, steps.get(i+1).y-steps.get(i).y);
+//			System.out.printf("%d: ", c++);
+//			m.printMove();
+			moves.add(m);
+		}
+		return moves;
+	}
+
+	private static boolean findValidPath(ArrayList<Square> steps, int startX, int startY, int endX, int endY, int count){
+//		board.printBoardCount();
 		knight.currentX = startX;
 		knight.currentY = startY;
 		
 		if (knight.currentX == endX && knight.currentY == endY){
-			board.printBoardCount();
+//			board.printBoardCount();
 			return true;
 		}
 		
@@ -119,7 +139,7 @@ public class Questions {
 		for(Square nb : nbrs) {
 			board.setSquareCount(nb.x, nb.y, count);
 			if(findValidPath(steps, nb.x, nb.y, endX, endY, count+1)){
-				Move m = new Move(nb.x, nb.y);				
+				Square m = new Square(nb.x, nb.y);				
 				steps.add(m);
 				return true;
 			}
